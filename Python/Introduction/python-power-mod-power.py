ab8c2f46bc2a333d152ad477497a8247f28b1948
Daffa Rozano <daffarozano@gmail.com>
2021-02-18 18:59:26 +0700

    invalid syntax

--- Python/Introduction/python-power-mod-power.py
+++ Python/Introduction/python-power-mod-power.py
@@ -1,6 +1,6 @@
-a = int(raw_input())
-b = int(raw_input())
-m = int(raw_input())
+a = int(input())
+b = int(input())
+m = int(input())
 
-print pow(a,b)
-print pow(a,b,m)
+print (pow(a,b))
+print (pow(a,b,m))
